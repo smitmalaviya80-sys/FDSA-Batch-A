9f5e36dfcee2b08cda8cabf4f2ca5fa5a7d09112
smit malaviya <smitmalaviya80@gmail.com>
2026-08-02 21:32:43 +0530

    selection shorting

---
+++ practical-3/insertion.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={5,4,3,2,1};
+    for(int i=0;i<5;i++){
+        int min=i;
+        for(int j=i+1;j<5;j++){
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        }
+
+        int temp=arr[i];
+        arr[i]=arr[min];
+        arr[min]=temp;
+    }
+
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+}
